205c34039c410d1d1d1bcd1f81a55f7c1e44a46a
Yan Almeida <yandealmeida07@outlook.com>
2025-12-20 04:47:46 -0300

    Commit inicial do projeto Spring Boot

---
+++ src/test/java/dev/YanAlmeida/CadastroDeAlunos/CadastroDeAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.YanAlmeida.CadastroDeAlunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
