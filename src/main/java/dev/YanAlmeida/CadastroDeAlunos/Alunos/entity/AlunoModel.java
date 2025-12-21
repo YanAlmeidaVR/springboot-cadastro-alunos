c4bd6bdd881e84e13e09e2a21b817266826f6156
Yan Almeida <yandealmeida07@outlook.com>
2025-12-21 03:18:49 -0300

    feat: Migrations via Flyway implementada

--- src/main/java/dev/YanAlmeida/CadastroDeAlunos/Alunos/entity/AlunoModel.java
+++ src/main/java/dev/YanAlmeida/CadastroDeAlunos/Alunos/entity/AlunoModel.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 @Entity
 @Table(name = "tb_alunos")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 
@@ -27,4 +26,44 @@ public class AlunoModel {
 
     @Column(name = "idade")
     private int idade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
---
+++ src/main/resources/db/migration/V2__insert_dados_iniciais_aluno.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_alunos (nome, cpf, email, idade) VALUES
+('João Silva', '12345678901', 'joao.silva@email.com', 20),
+('Maria Oliveira', '98765432100', 'maria.oliveira@email.com', 22),
+('Carlos Pereira', '45678912300', 'carlos.pereira@email.com', 19);
